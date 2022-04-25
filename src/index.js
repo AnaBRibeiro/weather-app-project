a161ac7cd9de12f253e97ac5489e4c186368287c
Ana Ribeiro <anabrazribeiro@gmail.com>
2022-04-25 11:32:50 +0100

    Displayed current weather info (temp in Celsius)

--- src/index.js
+++ src/index.js
@@ -23,11 +23,33 @@ function displayDayAndTime(timestamp) {
 }
 
 function displayData(response) {
-  let dayAndTime = document.querySelector("#day-and-time");
-  dayAndTime.innerHTML = displayDayAndTime(response.data.dt * 1000);
+  let cityNameElement = document.querySelector("#city-name");
+  cityNameElement.innerHTML = response.data.name;
+
+  let dayAndTimeElement = document.querySelector("#day-and-time");
+  dayAndTimeElement.innerHTML = displayDayAndTime(response.data.dt * 1000);
+
+  let weatherDescriptionElement = document.querySelector(
+    "#weather-description"
+  );
+  weatherDescriptionElement.innerHTML = response.data.weather[0].description;
+
+  let weatherHumidityElement = document.querySelector(
+    "#value-weather-humidity"
+  );
+  weatherHumidityElement.innerHTML = response.data.main.humidity;
+
+  let weatherWindSpeedElement = document.querySelector(
+    "#value-weather-wind-speed"
+  );
+  weatherWindSpeedElement.innerHTML = Math.round(response.data.wind.speed);
+
+  let temperatureElement = document.querySelector("#value-temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
 }
 
 let apiKey = `6e2e6ba4445c2a3f7c96186354d36ba3`;
 let cityName = `New York`;
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+let unit = `metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&&units=${unit}`;
 axios.get(apiUrl).then(displayData);
