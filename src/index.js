984e03da98c4a8084103f930eeb19c2815a006fe
Ana Ribeiro <anabrazribeiro@gmail.com>
2022-04-26 11:52:43 +0100

    Implemented temperature unit conversion

--- src/index.js
+++ src/index.js
@@ -44,12 +44,43 @@ function displayData(response) {
   );
   weatherWindSpeedElement.innerHTML = Math.round(response.data.wind.speed);
 
+  celsiusTemperature = Math.round(response.data.main.temp);
+
   let temperatureElement = document.querySelector("#value-temperature");
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  temperatureElement.innerHTML = celsiusTemperature;
 }
+
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#value-temperature");
+  fahreinheitLinkElement.classList.remove("unit-conversion-link-fahreinheit");
+  fahreinheitLinkElement.classList.add("unit-conversion-link-celsius");
+  celsiusLinkElement.classList.remove("unit-conversion-link-celsius");
+  celsiusLinkElement.classList.add("unit-conversion-link-fahreinheit");
+  let fahreinheitTemperature = Math.round(celsiusTemperature * 1.8 + 32);
+  temperatureElement.innerHTML = fahreinheitTemperature;
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#value-temperature");
+  celsiusLinkElement.classList.remove("unit-conversion-link-fahreinheit");
+  celsiusLinkElement.classList.add("unit-conversion-link-celsius");
+  fahreinheitLinkElement.classList.remove("unit-conversion-link-celsius");
+  fahreinheitLinkElement.classList.add("unit-conversion-link-fahreinheit");
+  temperatureElement.innerHTML = celsiusTemperature;
+}
+
+let celsiusTemperature = null;
 
 let apiKey = `6e2e6ba4445c2a3f7c96186354d36ba3`;
 let cityName = `New York`;
 let unit = `metric`;
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&&units=${unit}`;
 axios.get(apiUrl).then(displayData);
+
+let fahreinheitLinkElement = document.querySelector("#fahreinheit-link");
+fahreinheitLinkElement.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLinkElement = document.querySelector("#celsius-link");
+celsiusLinkElement.addEventListener("click", displayCelsiusTemperature);
